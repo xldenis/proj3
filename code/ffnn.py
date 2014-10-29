5a2ffe336ba36ee436bd4f99788612f34102cd79
Xavier Denis <xldenis@gmail.com>
2014-10-29 12:50:33 -0400

    Translate ruby to python.

---
+++ code/ffnn.py
@@ -0,0 +1,51 @@
+from math import exp
+
+def sigmoid(x):
+  return 1 / (1 + exp(-1*x))
+
+def evaluate(weights, example):
+  vec = example
+  for mat in weights:
+    vec = [ sigmoid(sum([a*b for a,b in zip(row,vec)])) for row in mat]
+  return vec
+
+def backprop(weights, example, label):
+  outputs = []
+  vec = example
+  for mat in weights:
+    vec = [ sigmoid(sum([a*b for a,b in zip(row,vec)])) for row in mat]
+    outputs.append(vec)
+  delta = [0] * len(weights)
+  delta[len(weights) - 1] = [ o*(1-o)*(l-o) for l,o in zip(label, outputs[-1]) ]
+  for idx in range(len(weights)-1)[::-1]:
+    vec = outputs[idx]
+    layer_update = [
+      sum([a*b for a,b in zip(row, delta[idx+1])])
+      for row in transpose(weights[idx+1])]
+    delta[idx] = [o*(1-o)*u for o,u in zip(vec,layer_update)]
+
+  for idx in range(len(weights)):
+    update = sum([ a*b for a,b in zip(delta[idx], outputs[idx])])
+    for rid in range(len(weights[idx])):
+      weights[idx][rid] = [n + update for n in weights[idx][rid]]
+  return weights 
+
+def transpose(mat):
+  return map(list, zip(*mat))
+
+def init_weights(layers):
+  mats = []
+  for i in range(len(layers)-1):
+    mat = []
+    mats.append(mat)
+    for j in range(layers[i+1]):
+      mat.append([1 for k in range(layers[i])])
+  return mats
+
+def main():
+  w = init_weights([1,2,1,1])
+  print w
+  print evaluate(w, [1])
+  print backprop(w, [1], [0])
+
+if  __name__ =='__main__':main()
